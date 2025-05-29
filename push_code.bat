93e59614c1475cf2378912db46a4a3510263322d
ttnlwayb <127585129+ttnlwayb@users.noreply.github.com>
2025-05-29 22:53:20 +0800

    auto commit 2025/05/29 週四 22:53:20.71

---
+++ push_code.bat
@@ -0,0 +1,10 @@
+﻿set GIT_BIN=C:\Program Files\Git\cmd\git.exe
+
+cd /d D:\IT\github\futures_order
+
+set MESSAGE=auto commit %date% %time%
+
+"%GIT_BIN%" add .
+"%GIT_BIN%" commit -m "%MESSAGE%"
+"%GIT_BIN%" push origin
+
